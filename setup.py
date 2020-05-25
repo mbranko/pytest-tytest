876d5e9fb217798225a439c1fe1ebe8edc8e2d2d
Branko Milosavljevic <mbranko@uns.ac.rs>
2020-05-25 21:45:05 +0200

    Bump version: 0.1.8 → 0.1.9

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def read(fname):
 
 setup(
     name='pytest-tytest',
-    version='0.1.8',
+    version='0.1.9',
     author='Branko Milosavljevic',
     author_email='branko@typhoon-hil.com',
     maintainer='Branko Milosavljevic',
